9ba1d30ff263ac7be11fdf0db08a4d74268a1c8b
andreskull2 <andreskull1@gmail.com>
2025-05-06 10:39:06 -0300

    Criar a função para formatar a moeda

--- scripts.js
+++ scripts.js
@@ -41,8 +41,10 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
     // console.log("Convertendo:", amount, price, symbol) // ← Testa os dados recebidos
+    
     try {
-        description.textContent = `${symbol} 1 = ${price}`
+        // Exibindo a cotação da moeda selecionada
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
         // Aplica a classe que exibe o footer para mostrar o resultado
         footer.classList.add("show-result")
@@ -53,5 +55,14 @@ function convertCurrency(amount, price, symbol) {
         console.log(error)
         alert("Não foi possível converter. Tente novamente mais tarde.")
     }
+}
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value) {
     
+    // Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00)
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
 }
